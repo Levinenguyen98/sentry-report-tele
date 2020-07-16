60f9a9aeedb9b70c7a4a66a0762dc3e8b32e6b61
LevineNguyen98 <vinh.nguyen@ktek.io>
2020-07-16 15:02:43 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const telegramClient = require('./lib/telegram-client')
 const axios = require('axios')
 const config = require('./config')
 function run() {
-    cron.schedule(`*${config.PERIOD} * * * *`, () => {
+    cron.schedule(`${config.PERIOD} * * * *`, () => {
         axios.get(`https://sentry.io/api/0/organizations/${config.ORGANIZATIONS}/issues/?limit=25&project=${config.PROJECT}&query=is%3Aunresolved&shortIdLookup=1&statsPeriod=60m`, { params: {}, headers: { 'Authorization': config.AUTHORIZATION } }).then(response => {
             let listSendData = ''
             if(response.data.length > 0){
